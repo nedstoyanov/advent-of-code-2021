2fc995056658a8236573ebe4322fdac94e9775f0
Nedeltcho Stoyanov <nstoyanov@tyro.com>
2021-12-12 11:24:42 +1100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,60 @@
+import java.util.LinkedList
+
+data class SubState1(var count: Int, var previousDepth: Int) {
+
+    fun calculate(element: Int) : SubState1 {
+        if (element > this.previousDepth) {
+            this.count++
+        }
+        this.previousDepth = element
+        return this
+    }
+}
+
+data class SubState2(
+    var count: Int,
+    var previousDepthSum: Int,
+    val previousDepths: LinkedList<Int>
+    ) {
+
+    fun calculate(element: Int) : SubState2 {
+        this.previousDepths.addLast(element)
+
+        if (this.previousDepths.size == 4) {
+            this.previousDepths.removeFirst()
+        }
+
+        if (this.previousDepths.size == 3) {
+            val sum = this.previousDepths.sum()
+            if (sum > previousDepthSum) {
+                count++
+            }
+            previousDepthSum = sum
+        }
+
+        return this
+    }
+}
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toInt() }
+            .fold(SubState1(0, Int.MAX_VALUE)) { subState: SubState1, element: Int ->
+                subState.calculate(element)
+            }
+            .count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toInt() }
+            .fold(SubState2(0, Int.MAX_VALUE, LinkedList<Int>())) { subState: SubState2, element: Int ->
+                subState.calculate(element)
+            }
+            .count
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
